a3bb3150bce23932ed284d6d6c45122311c1d0b4
rodolfo comparsi <rodolfocomparsi@hotmail.com>
2023-03-29 08:07:50 -0300

    Initial Commit

---
+++ DietPlan/DietPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DietPlanApp.swift
+//  DietPlan
+//
+//  Created by Rodolfo Comparsi on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DietPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
